c4aaff6370ae23411ab41f1f76c76c5d5e720442
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-08 17:51:02 +0200

    Add files via upload

---
+++ ZRozw1_13/ZRozw1_13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRozw1_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            int suma = 0;
+            while (i<=n)
+            {
+                suma += i;
+                i++;
+            }
+            Console.WriteLine("Suma = {0}", suma);
+            Console.ReadKey();
+        }
+    }
+}
